f487ed43ee61a1681e3bf76fb73c7dda2cb9b378
Forstner Michael <michael.forstner@cpg.de>
2015-11-13 16:48:36 +0100

    Remove dependency to REST assured - now it's a pure spring project

--- app/src/test/java/demo/ApplicationTest.java
+++ app/src/test/java/demo/ApplicationTest.java
@@ -1,14 +1,20 @@
 package demo;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static com.jayway.restassured.RestAssured.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -18,21 +24,23 @@ public class ApplicationTest {
     @Value("${local.server.port}")
     private int httpPort;
 
-    @Test
-    public void testUS() {
-        given().port(httpPort)
-                .when()
-                .get("/us")
-                .then()
-                .statusCode(HttpStatus.OK.value());
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
 
     @Test
-    public void testDE() {
-        given().port(httpPort)
-                .when()
-                .get("/de")
-                .then()
-                .statusCode(HttpStatus.OK.value());
+    public void testUS() throws Exception {
+        this.mockMvc.perform(get("/us")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDE() throws Exception {
+        this.mockMvc.perform(get("/de")).andExpect(status().isOk());
     }
 }
